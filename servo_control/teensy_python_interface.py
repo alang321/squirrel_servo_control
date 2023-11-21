ec0be2f4dfe004c1f4b025e4fb83f1356711780a
Anton Lang <34992832+alang321@users.noreply.github.com>
2023-11-21 21:32:34 +0100

    Update teensy_python_interface.py

--- servo_control/teensy_python_interface.py
+++ servo_control/teensy_python_interface.py
@@ -100,7 +100,8 @@ def cmd_getAll(servo_id):
     writeToSerial(struct_var)
 
 def process_speed_reply():
-    unpacked_reply = struct.unpack(replystruct_get_speed_format, struct.calcsize(replystruct_get_speed_format))
+    buffer = serial_connection.read(struct.calcsize(replystruct_get_speed_format))
+    unpacked_reply = struct.unpack(replystruct_get_position_format, buffer)
     servo_id = unpacked_reply[0]
     speed = unpacked_reply[1]
 
@@ -109,7 +110,8 @@ def process_speed_reply():
     print("speed: ", speed)
 
 def process_position_reply():
-    unpacked_reply = struct.unpack(replystruct_get_position_format, struct.calcsize(replystruct_get_position_format))
+    buffer = serial_connection.read(struct.calcsize(replystruct_get_position_format))
+    unpacked_reply = struct.unpack(replystruct_get_position_format, buffer)
     servo_id = unpacked_reply[0]
     position = unpacked_reply[1]
 
@@ -118,6 +120,7 @@ def process_position_reply():
     print("position: ", position)
 
 def process_volt_reply():
+    buffer = serial_connection.read(struct.calcsize(replystruct_get_volt_format))
     unpacked_reply = struct.unpack(replystruct_get_volt_format, struct.calcsize(replystruct_get_volt_format))
     servo_id = unpacked_reply[0]
     volt = unpacked_reply[1]
@@ -127,7 +130,8 @@ def process_volt_reply():
     print("volt: ", volt)
 
 def process_temp_reply():
-    unpacked_reply = struct.unpack(replystruct_get_temp_format, struct.calcsize(replystruct_get_temp_format))
+    buffer = serial_connection.read(struct.calcsize(replystruct_get_temp_format))
+    unpacked_reply = struct.unpack(replystruct_get_temp_format, buffer)
     servo_id = unpacked_reply[0]
     temp = unpacked_reply[1]
 
@@ -136,7 +140,8 @@ def process_temp_reply():
     print("temp: ", temp)
 
 def process_is_moving_reply():
-    unpacked_reply = struct.unpack(replystruct_get_is_moving_format, struct.calcsize(replystruct_get_is_moving_format))
+    buffer = serial_connection.read(struct.calcsize(replystruct_get_is_moving_format))
+    unpacked_reply = struct.unpack(replystruct_get_is_moving_format, buffer)
     servo_id = unpacked_reply[0]
     is_moving = unpacked_reply[1]
 
@@ -145,7 +150,8 @@ def process_is_moving_reply():
     print("is_moving: ", is_moving)
 
 def process_all_reply():
-    unpacked_reply = struct.unpack(replystruct_get_all_format, struct.calcsize(replystruct_get_all_format))
+    buffer = serial_connection.read(struct.calcsize(replystruct_get_all_format))
+    unpacked_reply = struct.unpack(replystruct_get_all_format, buffer)
     servo_id = unpacked_reply[0]
     position = unpacked_reply[1]
     speed = unpacked_reply[2]
