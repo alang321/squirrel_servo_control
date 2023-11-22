af98cd6d06957a264924d6d80539d861c93ca021
Anton Lang <34992832+alang321@users.noreply.github.com>
2023-11-22 14:46:44 +0100

    Update squirrel_servo_node.py

--- src/squirrel_servo_node.py
+++ src/squirrel_servo_node.py
@@ -32,9 +32,9 @@ def teensy_comm():
 
     teensy.cmd_setSerialPort(1)
 
-    #rospy.Subscriber("servo_set_speed", servo_speed, callback_speed)
-    #rospy.Subscriber("servo_set_position", servo_position, callback_position)
-    #rospy.Subscriber("motor_set_speed", motor_speed, callback_motor_speed)
+    rospy.Subscriber("servo_set_speed", servo_speed, callback_speed)
+    rospy.Subscriber("servo_set_position", servo_position, callback_position)
+    rospy.Subscriber("motor_set_speed", motor_speed, callback_motor_speed)
 
     #duration = 1/6/len(servo_list)
     duration = 10
