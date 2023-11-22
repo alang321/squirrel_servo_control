c7db59e7ff32b230e7de3080afd7dc658172543d
Anton Lang <34992832+alang321@users.noreply.github.com>
2023-11-22 10:58:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-packages=['teensy_scripts'],
+packages=['scripts'],
 package_dir={'': 'src'},
 )
 setup(**setup_args)
