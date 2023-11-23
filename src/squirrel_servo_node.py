49a2c78e0c5fe12cc28ffb6fad7c89d705a74ebf
Anton Lang <34992832+alang321@users.noreply.github.com>
2023-11-23 03:59:47 +0100

    Update squirrel_servo_node.py

--- src/squirrel_servo_node.py
+++ src/squirrel_servo_node.py
@@ -35,7 +35,7 @@ def teensy_comm():
     rospy.Subscriber("motor_set_speed", motor_speed, callback_motor_speed)
 
     duration = 1/6/len(servo_list)
-    rospy.Timer(rospy.Duration(duration), callback_timer)
+    #rospy.Timer(rospy.Duration(duration), callback_timer)
 
     rospy.spin()
 
